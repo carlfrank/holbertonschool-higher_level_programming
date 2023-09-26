dc4925a761872cdf23e9615dc013975a607229f8
carlfrank <carl-frank7@hotmail.com>
2023-09-26 14:18:07 -0400

    Pow Pow

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,1 @@
 #!/usr/bin/python3
-def print_comb3():
-    for i in range(0, 10):
-        for j in range(0, 10):
-            if i < j:
-                print("{}{}".format(i, j), end="")
-                if i < 8:
-                    print(", ", end="")
-    print("")
-
