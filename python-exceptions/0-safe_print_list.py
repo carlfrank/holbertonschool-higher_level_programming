1b238d254ea0970eb8cb7ef71109bc89fe47ccb1
carlfrank <carl-frank7@hotmail.com>
2023-10-04 12:03:36 -0400

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-        print()
-        return i + 1
-    except:
-        print()
-        return i
+    except IndexError:
+        pass
+    print()
+    return i + 1 if i < x else x
