83cc57665009f736e76808d019d3f0f3a3a8f601
carlfrank <carl-frank7@hotmail.com>
2023-10-13 08:37:45 -0400

    test indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,20 +3,15 @@
 
 
 def text_indentation(text):
-    """
-    Function that prints a text
-    with 2 new lines after each
-    of the chracters above.
-    First we check that what its
-    passed is a str, then proceed to check and print.
-    """
-    if type(text) is not str:
-        raise TypeError('text must be a string')
-    for let in range(len(text)):
-        if text[let] == '.' or text[let] == '?' or text[let] == ':':
-            print(text[let])
-            print()
-        elif text[let] == " " and text[let - 1] in ['.', '?', ':']:
-            continue
-        else:
-            print(text[let], end="")
+    """Prints a text, adding 2 new lines after each ., ? and :."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n")
+            if i + 1 < len(text) and text[i + 1] == ' ':
+                i += 1
+        i += 1
