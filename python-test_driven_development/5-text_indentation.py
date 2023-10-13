1a3b5562fe25d406cd737c9577a43be53c8630e6
carlfrank <carl-frank7@hotmail.com>
2023-10-13 12:19:11 -0400

    text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-"""Finds the max integer in a list"""
+"""This module will have a function that will print a text"""
 
 
-def max_integer(list=[]):
-    """max steel"""
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return (result)
+def text_indentation(text):
+    """
+    Function that prints a text
+    with 2 new lines after each
+    of the chracters above.
+    First we check that what its
+    passed is a str, then proceed to check and print.
+    """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    skip_space = False
+    # Add a flag to skip spaces after special characters
+    for let in text:
+        if skip_space and let == " ":
+            continue
+        elif let in ['.', '?', ':']:
+            print(let, end="")
+            print("\n")
+            skip_space = True
+            # Enable the flag after special characters
+        else:
+            print(let, end="")
+            skip_space = False
+            # Disable the flag when non-space character is encountered
