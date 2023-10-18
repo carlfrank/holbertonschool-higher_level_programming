a08cac08eb396faefcb595249380f1185c4a16b9
carlfrank <carl-frank7@hotmail.com>
2023-10-18 10:48:19 -0400

    9 again

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""Write a class Rectangle that inherits from BaseGeometry"""
+"""
+This module have a class called Rectangle that
+is a child of BaseGeometry
+"""
 
 
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
+    """
+    Rectangle is a child class from BaseGeometry
+    """
     def __init__(self, width, height):
-
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """
+        Function that validates the weight & height
+        """
         self.__width = width
         self.__height = height
-
-    def area(self):
-
-        return self.__width * self.__height
-
-    def __str__(self):
-
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
