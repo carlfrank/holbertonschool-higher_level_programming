ea1096fdc38e2afa1fcf18fa17e04446c4d97b90
carlfrank <carl-frank7@hotmail.com>
2023-10-19 16:17:04 -0400

    class to json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to create a class from JSON file"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+    """
+    return {key: value for key, value in obj.__dict__.items()}
