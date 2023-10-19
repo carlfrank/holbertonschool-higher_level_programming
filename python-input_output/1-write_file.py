ebcd449d13de9cebff41922b22c0f50975c59f14
carlfrank <carl-frank7@hotmail.com>
2023-10-19 14:21:53 -0400

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding='utf-8') as file:
-        file.write(text)
-
-    return len(text)
+    """ Write to a file"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
