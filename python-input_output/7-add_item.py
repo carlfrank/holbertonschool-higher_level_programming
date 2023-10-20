557b0dffb50d05f7fc7948911769721adefa2b06
carlfrank <carl-frank7@hotmail.com>
2023-10-19 21:37:50 -0400

    Class to json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     my_list = load_from_json_file("add_item.json")
+except:
+    my_list = []
 for i in sys.argv[1:]:
     my_list.append(i)
     save_to_json_file(my_list, "add_item.json")
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Module to create a class from JSON file"""
+"""Module convert to json"""
 
 
 def class_to_json(obj):
-    """
-    Returns the dictionary description with simple data structure
-    for JSON serialization of an object.
-    """
-    return {key: value for key, value in obj.__dict__.items()}
+    """ returns a dictionaty description with simple data structure"""
+    return obj.__dict__
