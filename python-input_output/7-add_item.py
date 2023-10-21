f54460af3a002824246f634c08b01b7f5393554d
carlfrank <carl-frank7@hotmail.com>
2023-10-20 21:08:23 -0400

    Pascal Triangle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Module adds all arguments to a Python list,and then save them to a file"""
-
+"""Module for add_item method."""
 
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
