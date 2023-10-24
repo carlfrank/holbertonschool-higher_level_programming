c255bc3fc4e48da1b4a5df420834a78e3e45e116
carlfrank <carl-frank7@hotmail.com>
2023-10-24 10:58:21 -0400

    rectangle testing

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,33 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """gets private instance attribute width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """sets private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """gets private instance attribute height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """sets private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
