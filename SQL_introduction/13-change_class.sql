d2b0b09da1d27d2a8250f13db46aa0b5f2c5591b
carlfrank <carl-frank7@hotmail.com>
2023-11-20 20:33:18 -0500

    Score too low

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
--- Script to delete records from a table in a MySQL database based on a condition
+-- Script to delete records from a table in a MySQL database based on a condition.
 DELETE FROM second_table WHERE score <= 5;
