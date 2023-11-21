b3bb797105bfd865b99dd262b9fc4046344139b0
carlfrank <carl-frank7@hotmail.com>
2023-11-21 13:15:11 -0500

    Read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- 2-create_read_user.sql
+
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the hbtn_0d_2 database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
