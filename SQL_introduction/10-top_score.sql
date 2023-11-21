fa177fe6842a6bfb8e6a927bc67ef10b558a36a4
carlfrank <carl-frank7@hotmail.com>
2023-11-20 20:31:46 -0500

    List by best

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- Script to list all records from a specific table in a MySQL database
+-- Script to list all records from a specific table in a MySQL database.
 SELECT score, name FROM second_table ORDER BY score DESC;
