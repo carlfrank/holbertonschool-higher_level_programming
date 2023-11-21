873a9f4e168f9e09c5e36b56d7d240a31feae0c8
carlfrank <carl-frank7@hotmail.com>
2023-11-20 20:29:31 -0500

    List all in table

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- Script to list all rows and fields from a specific table in a MySQL database
+-- Script to list all rows and fields from a specific table in a MySQL database.
 SELECT * FROM first_table;
