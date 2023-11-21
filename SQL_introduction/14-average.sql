c626d5d28babc0cf546d1f1ad063b54f3918e447
carlfrank <carl-frank7@hotmail.com>
2023-11-20 20:33:44 -0500

    Average

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
--- Script to compute the average of a column in a table in a MySQL database
+-- Script to compute the average of a column in a table in a MySQL database.
 SELECT AVG(score) AS average FROM second_table;
