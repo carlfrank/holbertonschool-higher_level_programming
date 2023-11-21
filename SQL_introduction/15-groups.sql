c5b67ff0346cecbdb714432580098aaa0a2ddad7
carlfrank <carl-frank7@hotmail.com>
2023-11-20 20:34:08 -0500

    Number by score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Script to list the number of records with the same value in a column in a table in a MySQL database
--- and to sort the results
+-- and to sort the results.
 SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
