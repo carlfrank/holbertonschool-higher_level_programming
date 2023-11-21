8255819a6cba3c51ce2a11929f4fbe5cc3b95583
carlfrank <carl-frank7@hotmail.com>
2023-11-20 20:41:30 -0500

    My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Script to show privileges of specific users in MySQL
+
+-- Show privileges for user 'user_0d_1'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
