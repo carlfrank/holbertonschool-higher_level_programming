f06fa66a03be6adac4e1500f6be7b7efa529df7c
carlfrank <carl-frank7@hotmail.com>
2023-11-24 13:05:55 -0500

    cities by state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
 """
-Displays all states from db with name starting with 'N'
+Module to list all cities from database hbtn_0e_4_usa
 """
+
+
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host='localhost',
+if __name__ == "__main__":
+    """Function that lists all cities from database hbtn_0e_4_usa"""
+
+    cnx = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        charset="utf8",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE NAME LIKE 'N%';")
-    states = cur.fetchall()
+        database=sys.argv[3])
 
-    for state in states:
-        print(state)
+    cur = cnx.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+
+    cnx.close()
