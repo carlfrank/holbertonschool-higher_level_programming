aaf15940285ca9723cbbe51bc213a252c7bb0531
carlfrank <carl-frank7@hotmail.com>
2023-11-24 12:57:38 -0500

    Safe Filter

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Displays all states from db with name starting with 'N'
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    databases = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=databases,
+        port=3306
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE NAME LIKE %s ORDER BY states.id;", (state_name_searched,))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
