027cf75ddbb17d9f046b8dd3d06377e3fdc55ded
carlfrank <carl-frank7@hotmail.com>
2023-11-24 13:01:31 -0500

    Changing the code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""
-Displays all states from db with name starting with 'N'
-"""
+"""take arguments and display all values in the states table"""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    databases = sys.argv[3]
-    state_name_searched = sys.argv[4]
-
+if __name__ == "__main__":
     db = MySQLdb.connect(
-        host='localhost',
-        user=username,
-        passwd=password,
-        db=databases,
-        port=3306
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
     )
 
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM ORDER BY states.id;", (state_name_searched,))
-    states = cur.fetchall()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
+                ORDER BY id ASC", (argv[4],))
 
-    for state in states:
-        print(state)
+    rows = cur.fetchall()
+
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+
+    cur.close()
+    db.close()
