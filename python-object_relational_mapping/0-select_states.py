75ab639759853fece1eaa7576e3d5323c9332bb9
carlfrank <carl-frank7@hotmail.com>
2023-11-24 12:18:45 -0500

    Changing the code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-Displays all states from db.
-"""
-
+"""Module to list all states from database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
+
 
 def main():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=username, passwd=password, db=database)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
@@ -21,6 +20,7 @@ def main():
 
     cur.close()
     db.close()
+
 
 if __name__ == "__main__":
     main()
