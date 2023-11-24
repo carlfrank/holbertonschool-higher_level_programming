3e6d55564c17ea47a766b7cdebf7fdc2d97eb5d7
carlfrank <carl-frank7@hotmail.com>
2023-11-24 12:56:02 -0500

    adding chmod 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module to take user input and display all values in the states table of
-hbtn_0e_0_usa where name matches the argument.
+hbtn_0e_0_usa where name matches the argument
 """
 
 
