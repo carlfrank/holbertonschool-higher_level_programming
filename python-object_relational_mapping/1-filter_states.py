11032151e2180de0a44dc81ed431a06ba1e0fa35
carlfrank <carl-frank7@hotmail.com>
2023-11-24 12:44:42 -0500

    Filter State

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,45 @@
 #!/usr/bin/python3
 """
-Displays all states from db with name starting with 'N'
+Module to list all states with a name that starts with a
+certain character from database hbtn_0e_0_usa.
 """
+
+
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host='localhost',
+if __name__ == "__main__":
+    """
+    Function that lists all states with a name that starts with a
+    certain character from database hbtn_0e_0_usa.
+    """
+
+    # Connecting to a MySQL database.
+    cnx = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        charset="utf8",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE NAME LIKE 'N%';")
-    states = cur.fetchall()
+        database=sys.argv[3])
 
-    for state in states:
-        print(state)
+    # Making cursor obj for execution.
+    cur = cnx.cursor()
+
+    # Executing query.
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    # Obtaining query results.
+    query_rows = cur.fetchall()
+
+    # Printing results.
+    for row in query_rows:
+        if row[1][0] == 'N':
+            print(row)
+
+    # Close cursor.
+    cur.close()
+
+    # Close connection to database.
+    cnx.close()
